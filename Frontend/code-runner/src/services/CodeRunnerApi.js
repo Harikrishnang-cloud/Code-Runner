7efdf4a050ca021d3cd5dd19d2a2d54b17ec8319
Harikrishnan G <harikrishnangmonkuttan@gmail.com>
2025-12-20 11:39:56 +0530

    Update .gitignore, refactor API endpoint, and remove package.json

--- Frontend/code-runner/src/services/CodeRunnerApi.js
+++ Frontend/code-runner/src/services/CodeRunnerApi.js
@@ -1,5 +1,5 @@
 export async function runCodeApi(code,language) {
-  const res = await fetch("http://localhost:1729/run", {
+  const res = await fetch(import.meta.env.VITE_BASEURL, {
     method: "POST",
     headers: { "Content-Type": "application/json" },
     body: JSON.stringify({ code,language })
