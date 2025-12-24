8550192e01e369089b2fd350d98a17db0b54e78a
Harikrishnan G <harikrishnangmonkuttan@gmail.com>
2025-12-24 16:56:58 +0530

    time update

--- Frontend/code-runner/src/pages/EditorPage.jsx
+++ Frontend/code-runner/src/pages/EditorPage.jsx
@@ -39,7 +39,7 @@ function EditorPage() {
       })
     }
     infoToast()
-    const interval = setInterval(infoToast,15*60*1000)
+    const interval = setInterval(infoToast,1*60*1000)
     return()=>clearInterval(interval)
   },[])
 
